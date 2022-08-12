202a0e76a5878dfab8dacba018be00a5207af2cc
cecilialsantin <chikhakituti@gmail.com>
2022-08-12 16:02:54 -0300

    Practica typscript

---
+++ objetos.js
@@ -0,0 +1,7 @@
+"use strict";
+let porfolio = {
+    Nombre: "",
+    Apellido: "",
+    Acercade: "",
+    Titulos: ["", ""],
+};
---
+++ objetos.ts
@@ -0,0 +1,14 @@
+interface Persona{
+    Nombre: string;
+    Apellido: string;
+    Acercade:string;
+    Titulos:[string,string];
+}
+
+let porfolio: Persona={
+    Nombre:"",
+    Apellido:"",
+    Acercade:"",
+    Titulos:["", ""],
+}
+
